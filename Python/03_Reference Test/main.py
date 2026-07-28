5cd2e5bb68e158a9f4637441989095e63e62a37c
Manas-2007 <patidarmanas01@gmail.com>
2026-07-28 15:36:59 +0530

    Reference Test

---
+++ Python/03_Reference Test/main.py
@@ -0,0 +1,13 @@
+# Test-1
+list1=[1,2,3,4]
+list2=[1,2,3,4]
+print("Separate Reference :")
+print("1. Value Comparison : ",list1==list2)
+print("2. Reference Comparison : ",list1 is list2)
+
+# Test-2
+list3=[1,2,3,4]
+list4=list3
+print("\nSame Reference :")
+print("1. Value Comparison : ",list3==list4)
+print("2. Reference Comparison : ",list3 is list4)
